f95807fbd8f13c70b6d00aed46a30d52d5e621c8
Richard Cook <rcook@rcook.org>
2020-03-18 10:54:56 -0700

    Generic version of eval

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,18 @@
-use rlua::{Lua, Result};
+use rlua::{FromLuaMulti, Lua, Result};
 
-fn eval0(script: &str) -> Result<Vec<String>> {
+fn eval<R: for<'lua> FromLuaMulti<'lua>>(script: &str) -> Result<R> {
     Lua::new().context(|lua_ctx| lua_ctx.load(script).eval())
 }
-
-fn eval1(script: &str) -> Result<String> {
-    Lua::new().context(|lua_ctx| lua_ctx.load(script).eval())
-}
-
-fn eval2(script: &str) -> Result<()> {
-    Lua::new().context(|lua_ctx| lua_ctx.load(script).eval())
-}
-
-// How do I write a generic `eval` function?
-/*
-fn eval<R>(script: &str) -> Result<R> {
-    Lua::new().context(|lua_ctx| lua_ctx.load(script).eval())
-}
-*/
 
 fn main() -> Result<()> {
-    assert_eq!(eval0("return { \"one\", \"two\" }")?, vec!["one", "two"]);
     assert_eq!(
-        eval1("return \"Hello world\"")?,
+        eval::<Vec<String>>("return { \"one\", \"two\" }")?,
+        vec!["one", "two"]
+    );
+    assert_eq!(
+        eval::<String>("return \"Hello world\"")?,
         String::from("Hello world")
     );
-    eval2("print(\"Hello world\")")?;
+    eval("print(\"Hello world\")")?;
     Ok(())
 }
